6a592636dcd0c35b52d1d3d0454b53ddaa77e3a5
zwarunek <zacharywarunek8@gmail.com>
2022-04-12 13:17:03 -0400

    only set bearer token if using own api

--- src/app/features/compensation/existing-levels/existing-levels.component.ts
+++ src/app/features/compensation/existing-levels/existing-levels.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CompensationService} from "@features/compensation/compensation.service";
+import {Company} from "@features/compensation/company-search/company-search.component";
 
 @Component({
   selector: 'app-existing-levels',
@@ -9,11 +10,16 @@ import {CompensationService} from "@features/compensation/compensation.service";
 export class ExistingLevelsComponent implements OnInit {
 
 
-  @Input() set company(value: string) {
+  @Input() set company(value: Company | undefined) {
+    console.log('company', value);
     this.selectedCompany = value
-    this.getLevels()
+    if(this.selectedCompany?.name)
+      this.getLevels(this.selectedCompany.name)
+    else this.levels = []
   };
-  selectedCompany: string = ''
+  loading = false;
+  selectedCompany?: Company;
+
 
   levels: any[] = [];
   constructor(private compensationService: CompensationService) { }
@@ -21,10 +27,34 @@ export class ExistingLevelsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getLevels() {
-    this.compensationService.getExistingLevels(this.selectedCompany).subscribe((value: any) => {
+  getLevels(companyName: string) {
+    this.loading = true;
+    this.compensationService.getExistingLevels(companyName).subscribe((value: any) => {
       this.levels = value.body;
-      console.log(JSON.stringify(value.body, null, 2))
+      this.levels.forEach((level) => {
+        let stockCompensation = level.vestingSchedule.comp;
+        let firstYearVestingPercent = 0;
+        let firstYearBonus = 0;
+        let totalBonus = 0;
+        level.vestingSchedule.vestingYears.forEach((year: { year: number; percent: number; }) => {
+          if(year.year === 1){
+            firstYearVestingPercent = year.percent;
+          }
+        })
+        level.bonuses.forEach((year: { year: number; dollarBonus: number; }) => {
+          if(year.year === 1){
+            firstYearBonus = year.dollarBonus;
+          }
+          totalBonus += year.dollarBonus;
+        })
+
+        level.firstYearTC = (firstYearVestingPercent/100) * stockCompensation + firstYearBonus + level.salary;
+        level.avgTC =  (level.vestingSchedule.vestingYears.length>0?(stockCompensation / level.vestingSchedule.vestingYears.length):0)
+          + level.salary
+          + (level.bonuses.length>0?(totalBonus / level.bonuses.length):0);
+      })
+      console.log(JSON.stringify(this.levels, null, 2))
+      this.loading = false;
     });
 
   }
